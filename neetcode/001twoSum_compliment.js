a5eb951d9cde061dd26538f7a19d9734b77868aa
Howard Sun <komugi@gmail.com>
2024-05-14 12:12:02 -0400

    solved using complements
    Changes to be committed:
    	modified:   neetcode/001twoSum_compliment.js

--- neetcode/001twoSum_compliment.js
+++ neetcode/001twoSum_compliment.js
@@ -4,5 +4,17 @@
  * @return {number[]}
  */
 var twoSum = function (nums, target) {
-  const complimentaryMap = new Map();
+  const compMap = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+
+    if (compMap.has(complement)) {
+      return [compMap.get(complement), i];
+    } else {
+      compMap.set(nums[i], i);
+    }
+  }
 };
+
+console.log(twoSum([2, 3, 4], 6));
