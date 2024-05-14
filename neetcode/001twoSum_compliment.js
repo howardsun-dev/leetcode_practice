cb20d2a3e134709d2c6e9d5103a8ee5ebdeb45c0
Howard Sun <komugi@gmail.com>
2024-05-14 12:20:48 -0400

    Added comments for complement twoSum
    Changes to be committed:
    	modified:   neetcode/001twoSum_compliment.js
    
    Changes not staged for commit:
    	deleted:    neetcode/001twoSum_compliment.js
    
    Untracked files:
    	neetcode/001twoSum_complement.js

--- neetcode/001twoSum_compliment.js
+++ neetcode/001twoSum_compliment.js
@@ -4,15 +4,17 @@
  * @return {number[]}
  */
 var twoSum = function (nums, target) {
-  const compMap = new Map();
+  const compMap = new Map(); // Create a new map to store complements
 
+  // Loop through the array of numbers
   for (let i = 0; i < nums.length; i++) {
-    const complement = target - nums[i];
+    const complement = target - nums[i]; // Get the complement
 
     if (compMap.has(complement)) {
+      // Check if the complement is already in the map and return the indices is present in map
       return [compMap.get(complement), i];
     } else {
-      compMap.set(nums[i], i);
+      compMap.set(nums[i], i); // If not in the map, set the complement with index
     }
   }
 };
