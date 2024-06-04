a019179596a4c4dee359ffda6ad7572b6edfa338
Howard Sun <howardsun@pm.me>
2024-06-04 00:25:09 -0400

    started top k element

---
+++ neetcode/150topKElementInList.ts
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ *
+ * Input: nums = [1,2,2,3,3,3], k = 2
+ * Output: [2,3]
+ */
+const topKFrequent = (nums: number[], k: number): number[] => {
+  const map = new Map<number, number>();
+
+  return [2, 3];
+};
+
+const nums = [1, 2, 2, 3, 3, 3];
+const k = 2;
+console.log(topKFrequent(nums, k));
