3cd40f1818f7cf746c546f6ae0d27ac2bee1f0db
Howard Sun <howardsun@pm.me>
2024-06-04 21:35:13 -0400

    finished js topkelements in bucketsort

--- neetcode/150topKElementInList.ts
+++ neetcode/150topKElementInList.ts
@@ -6,33 +6,59 @@
  * Input: nums = [1,2,2,3,3,3], k = 2
  * Output: [2,3]
  */
-// Function definition for finding the top K frequent elements in an array
+
+// * Bucket Sort Approach
 const topKFrequent = (nums: number[], k: number): number[] => {
-  // Create a map to store the frequency of each number in the input array
-  const freqMap = new Map<number, number>();
+  const count: { [key: number]: number } = {};
+  const freq: number[][] = Array.from({ length: nums.length + 1 }, () => []);
 
-  // Iterate over each number in the input array
-  nums.forEach((num) => {
-    // If the number is already in the map, increment its count; otherwise, set it to 1
-    freqMap.set(num, (freqMap.get(num) || 0) + 1);
-  });
+  for (const n of nums) {
+    count[n] = (count[n] || 0) + 1;
+  }
 
-  // Convert the map entries into an array of [number, frequency] pairs
-  const freqArray = Array.from(freqMap.entries());
+  for (const n in count) {
+    freq[count[n]].push(parseInt(n));
+  }
 
-  // Sort the array in descending order based on the frequency of each number
-  freqArray.sort((a, b) => b[1] - a[1]);
+  const res: number[] = [];
+  for (let i = freq.length - 1; i > 0; i--) {
+    for (const n of freq[i]) {
+      res.push(n);
+      if (res.length === k) {
+        return res;
+      }
+    }
+  }
 
-  // Extract the top K elements based on their frequency
-  let topKElements = freqArray.slice(0, k).map((pair) => pair[0]);
-
-  // Sort the top K elements in ascending order
-  topKElements.sort((a, b) => a - b);
-
-  // Return the sorted list of top K frequent elements
-  return topKElements;
+  return res;
 };
 
-const nums = [1, 2, 2, 3, 3, 3]; // Input array
-const k = 2; // Number of top frequent elements to find
-console.log(topKFrequent(nums, k)); // Call the function and log the result
+// * Sorting Approach (O (n log n)
+
+// const topKFrequent = (nums: number[], k: number): number[] => {
+//   // Create a map to store the frequency of each number in the input array
+//   const freqMap = new Map<number, number>();
+
+//   nums.forEach((num) => {
+//     // If the number is already in the map, increment its count; otherwise, set it to 1
+//     freqMap.set(num, (freqMap.get(num) || 0) + 1);
+//   });
+
+//   const freqArray = Array.from(freqMap.entries());
+
+//   // Sort the array in descending order based on the frequency of each number
+//   freqArray.sort((a, b) => b[1] - a[1]);
+
+//   // Extract the top K elements based on their frequency
+//   let topKElements = freqArray.slice(0, k).map((pair) => pair[0]);
+
+//   // Sort the top K elements in ascending order
+//   topKElements.sort((a, b) => a - b);
+
+//   // Return the sorted list of top K frequent elements
+//   return topKElements;
+// };
+
+const nums = [1, 2, 2, 3, 3, 3];
+const k = 2;
+console.log(topKFrequent(nums, k));
