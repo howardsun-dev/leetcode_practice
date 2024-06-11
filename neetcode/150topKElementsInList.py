8444a89a5ac0835f79c3116d63505a7adc1acca6
Howard Sun <howardsun@pm.me>
2024-06-11 17:33:30 -0400

    150 in Python

--- neetcode/150topKElementsInList.py
+++ neetcode/150topKElementsInList.py
@@ -1,15 +1,25 @@
-from typing import List
-from collections import Counter
+from typing import List, Dict
 
 def top_k_elements(nums: List[int], k: int) -> List[int]:
-    """
-    :param nums: List[int]
-    :param k: int
-    :return: List[int]
-    """
+    count: Dict[int, int] = {}
+    freq: List[List[int]] = [[] for _ in range(len(nums) + 1)]
 
-    # Create freq dictionary
-    freq_dict = Counter(nums)
+    for n in nums:
+        count[n] = count.get(n, 0) + 1
 
-    # Convert dict to list of of (number, frequency) pairs
-    freq_list = list(freq_dict.items())
+    for n, c in count.items():
+        freq[c].append(n)
+
+    res: List[int] = []
+    for i in range(len(freq) - 1, 0, -1):
+        for n in freq[i]:
+            res.append(n)
+            if len(res) == k:
+                return res
+
+    return res
+
+# Example usage
+nums = [1, 2, 2, 3, 3, 3]
+k = 2
+print(top_k_elements(nums, k))
