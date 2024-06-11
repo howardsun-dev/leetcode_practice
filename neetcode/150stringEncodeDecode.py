ae20ba54816b165365e8abcd48a8f36075f802f8
Howard Sun <howardsun@pm.me>
2024-06-11 17:08:03 -0400

    Added python solution for 150

---
+++ neetcode/150stringEncodeDecode.py
@@ -0,0 +1,27 @@
+from typing import List
+
+def encode(strs: List[str]) -> str:
+    res = ''
+    for s in strs:
+        res += str(len(s)) + '#' + s
+    return res
+
+def decode(str: str) -> List[str]:
+    res = []
+    i = 0
+
+    while i < len(str):
+        j = str.index('#', i)
+        length = int(str[i:j])
+        print(length)
+        next_str = str[j + 1: j + 1 + length]
+        res.append(next_str)
+        i = j + 1 + length
+    return res
+
+
+input_strs = ['neet', 'code', 'love', 'you']
+encoded = encode(input_strs)
+print(encoded)
+decoded = decode(encoded)
+print(decoded)
