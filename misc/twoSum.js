e8f1570b6048220b167d5053c4bd2ae0c06f93b3
Howard Sun <howardsun@pm.me>
2024-10-13 16:09:19 -0400

    modified twoSum to use Map method instead of Object as hash

--- misc/twoSum.js
+++ misc/twoSum.js
@@ -1,19 +1,38 @@
+// function twoSum(nums, target) {
+//   const numList = {};
+
+//   for (let i = 0; i < nums.length; i++) {
+//     let compliment = target - nums[i];
+
+//     if (Object.hasOwn(numList, compliment)) {
+//       return [numList[compliment], i];
+//     }
+
+//     numList[nums[i]] = i;
+//   }
+//   return false;
+// }
+
+// let nums = [3, 4, 5, 6];
+// let target = 7;
+
 function twoSum(nums, target) {
-  const numList = {};
+  const numList = new Map();
 
   for (let i = 0; i < nums.length; i++) {
     let compliment = target - nums[i];
+    console.log(compliment);
 
-    if (Object.hasOwn(numList, compliment)) {
-      return [numList[compliment], i];
+    if (numList.has(compliment)) {
+      return [numList.get(compliment), i];
     }
 
-    numList[nums[i]] = i;
+    numList.set(nums[i], i);
   }
   return false;
 }
 
 let nums = [3, 4, 5, 6];
-let target = 7;
+let target = 11;
 
 console.log(twoSum(nums, target));
