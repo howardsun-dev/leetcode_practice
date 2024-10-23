700cce588b46bca5349fcf5e4caae68ee36d2936
Howard Sun <howardsun@pm.me>
2024-10-23 16:10:30 -0400

    redo

--- algomap/python/easy_2239_find_closest_number_to_zero.py
+++ algomap/python/easy_2239_find_closest_number_to_zero.py
@@ -2,21 +2,10 @@ from typing import List
 
 class Solution:
     def findClosestNumber(self, nums: List[int]) -> int:
-        closest = abs(nums[0])
-        # print(closest)
-
-        for num in nums:
-            # print(num)
-            curr_dist = abs(0 - num)
-
-            if (
-               curr_dist < abs(closest) or
-               (curr_dist == abs(closest) and num > closest)
-               ):
-                closest = num
+        closest = nums[0]
 
         return closest
-    
+
 solution = Solution()
 
 test = [-4,-2,1,4,8]
