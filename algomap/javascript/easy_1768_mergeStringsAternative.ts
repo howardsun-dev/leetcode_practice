e53056fceb609a9f95fe2640d08f602675fbf6ea
Howard Sun <howardsun@pm.me>
2024-10-30 15:23:10 -0400

    started leetcode 1768

---
+++ algomap/javascript/easy_1768_mergeStringsAternative.ts
@@ -0,0 +1,11 @@
+// You are given two strings word 1 and word 2. Merge the strings by adding letters in
+// alternating order, starting with word 1. If the string is longer than the other, append
+// the additional letters onto the end of the merged string.
+
+// Input: word1, word2 -- if one word is longer, append to the end after alternating
+// Output: merged string that's alternated
+
+function mergeAlternately(word1: string, word2: string): string {}
+
+const word1 = 'abc';
+const word2 = 'pqr';
