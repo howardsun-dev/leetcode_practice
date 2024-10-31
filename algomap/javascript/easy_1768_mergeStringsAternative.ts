35357e85fce94f5f9c59f55ad2444c3286b79940
Howard Sun <howardsun@pm.me>
2024-10-31 16:45:54 -0400

    alternative character leetcode

--- algomap/javascript/easy_1768_mergeStringsAternative.ts
+++ algomap/javascript/easy_1768_mergeStringsAternative.ts
@@ -5,7 +5,27 @@
 // Input: word1, word2 -- if one word is longer, append to the end after alternating
 // Output: merged string that's alternated
 
-function mergeAlternately(word1: string, word2: string): string {}
+function mergeAlternately(word1: string, word2: string): string {
+  let i: number = 0;
+  let j: number = 0;
+  let result: string = '';
+
+  while (i < word1.length || j < word2.length) {
+    if (i < word1.length) {
+      result += word1[i];
+      i++;
+    }
+
+    if (j < word2.length) {
+      result += word2[j];
+      j++;
+    }
+  }
+
+  return result;
+}
 
 const word1 = 'abc';
 const word2 = 'pqr';
+
+console.log(mergeAlternately(word1, word2));
