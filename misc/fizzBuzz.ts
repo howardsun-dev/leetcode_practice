70ce452c952f0544f6eb39f568755d4d4696ec8d
Howard Sun <howardsun@pm.me>
2024-12-05 09:52:39 -0500

    modified fizzbuzz to return results as array

--- misc/fizzBuzz.ts
+++ misc/fizzBuzz.ts
@@ -1,10 +1,15 @@
-function fizzBuzz(num: number): void {
+function fizzBuzz(num: number): string[] {
+  const results: string[] = [];
+
   for (let i = 1; i <= num; i++) {
     const fizz = i % 3 === 0 ? 'fizz' : '';
     const buzz = i % 5 === 0 ? 'Buzz' : '';
-    console.log(fizz + buzz || i);
+    // console.log(fizz + buzz || i);
+    results.push(fizz + buzz || i.toString());
   }
+
+  return results;
 }
 
 let num = 20;
-fizzBuzz(num);
+console.log(fizzBuzz(num));
