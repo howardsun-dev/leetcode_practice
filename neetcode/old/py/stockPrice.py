2fb1c55f407b4b5661183a1d468f96fcdf0a13a8
Howard Sun <howardsun@pm.me>
2025-09-24 16:34:16 -0400

    stock prices in python

--- neetcode/old/py/stockPrice.py
+++ neetcode/old/py/stockPrice.py
@@ -1,1 +1,17 @@
-import Collections from Lists
+def max_profit(prices):
+    left = 0
+    right = 1
+    max_profit = 0
+
+    while right < len(prices):
+        if prices[left] < prices[right]:
+            profit = prices[right] - prices[left]
+            max_profit = max(max_profit, profit)
+        else:
+            left += 1
+        right += 1
+    return max_profit
+
+
+prices = [7, 1, 5, 3, 6, 4]
+print(max_profit(prices))
